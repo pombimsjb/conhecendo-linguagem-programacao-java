adf5f7434c2df601814f42e558831f68b5d34dc6
GiancarloMolinari <itachisolid@gmail.com>
2023-03-26 16:04:30 -0300

    Upado Resumo Final da aula

---
+++ src/com/pombimsjb/sintaxejava/anatomiadeclasses/AnatomiaClasses4.java
@@ -0,0 +1,13 @@
+package com.pombimsjb.sintaxejava.anatomiadeclasses;
+
+public class AnatomiaClasses4 {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
--- src/com/pombimsjb/sintaxejava/anatomiadeclasses/MinhaClasse.java
+++ src/com/pombimsjb/sintaxejava/anatomiadeclasses/MinhaClasse.java
@@ -2,6 +2,13 @@ package com.pombimsjb.sintaxejava.anatomiadeclasses;
 
 public class MinhaClasse {
     public static void main(String[] args) {
+        String primeiroNome = "Giancarlo";
+        String segundoNome = "Molinari";
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
 
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método "+primeiroNome.concat(" ").concat(segundoNome);
     }
 }
